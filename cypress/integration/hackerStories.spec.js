cd05bbb0b2915550cf73cc69d94f0dc92f97593b
unknown <gustalef@gmail.com>
2022-06-07 14:54:33 -0300

    aula4 - finish exercise1

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -1,12 +1,74 @@
 /// <reference types="Cypress"/>
 
 describe('Hacker Stories', () => {
+  const initialTerm = 'React'
+  const newTerm = 'Cypress'
+
+  context('Hitting the real API', () => {
+    beforeEach(() => {
+      cy.intercept({
+        method: 'GET',
+        pathname: '**/search',
+        query: {
+          query: initialTerm,
+          page: '0'
+        }
+      }).as('getStories')
+  
+      cy.visit('/')
+      cy.wait('@getStories')
+    })
+
+    it('shows 20 stories, then the next 20 after clicking "More"', () => {
+      cy.intercept({
+        method: 'GET',
+        pathname: '**/search',
+        query: {
+          query: initialTerm,
+          page: '1'
+        }
+      }).as('getNextStories')
+      
+      cy.get('.item').should('have.length', 20)
+
+      cy.contains('More').click()
+      cy.wait('@getNextStories')
+
+      cy.get('.item').should('have.length', 40)
+    })
+    it('searches via the last searched term', () => {
+      cy.intercept(
+        'GET',
+        `**/search?query=${newTerm}&page=0`
+      ).as('getNewTermStories')
+
+      cy.get('#search')
+        .clear()
+        .type(`${newTerm}{enter}`)
+
+      cy.wait('@getNewTermStories')
+
+      cy.get(`button:contains(${initialTerm})`)
+        .should('be.visible')
+        .click()
+
+        cy.wait('@getStories')
+
+      cy.get('.item').should('have.length', 20)
+      cy.get('.item')
+        .first()
+        .should('contain', initialTerm)
+      cy.get(`button:contains(${newTerm})`)
+        .should('be.visible')
+    })
+  })
+
   beforeEach(() => {
     cy.intercept({
       method: 'GET',
       pathname: '**/search',
       query: {
-        query: 'React',
+        query: initialTerm,
         page: '0'
       }
     }).as('getStories')
@@ -14,7 +76,6 @@ describe('Hacker Stories', () => {
     cy.visit('/')
     cy.wait('@getStories')
   })
-
   it('shows the footer', () => {
     cy.get('footer')
       .should('be.visible')
@@ -28,24 +89,6 @@ describe('Hacker Stories', () => {
     // This is why this test is being skipped.
     // TODO: Find a way to test it out.
     it.skip('shows the right data for all rendered stories', () => {})
-
-    it('shows 20 stories, then the next 20 after clicking "More"', () => {
-      cy.intercept({
-        method: 'GET',
-        pathname: '**/search',
-        query: {
-          query: 'React',
-          page: '1'
-        }
-      }).as('getNextStories')
-      
-      cy.get('.item').should('have.length', 20)
-
-      cy.contains('More').click()
-      cy.wait('@getNextStories')
-
-      cy.get('.item').should('have.length', 40)
-    })
 
     it('shows only nineteen stories after dimissing the first story', () => {
       cy.get('.button-small')
@@ -73,8 +116,6 @@ describe('Hacker Stories', () => {
   })
 
   context('Search', () => {
-    const initialTerm = 'React'
-    const newTerm = 'Cypress'
 
     beforeEach(() => {
       cy.intercept(
@@ -117,25 +158,6 @@ describe('Hacker Stories', () => {
     })
 
     context('Last searches', () => {
-      it('searches via the last searched term', () => {
-        cy.get('#search')
-          .type(`${newTerm}{enter}`)
-
-        cy.wait('@getNewTermStories')
-
-        cy.get(`button:contains(${initialTerm})`)
-          .should('be.visible')
-          .click()
-
-          cy.wait('@getStories')
-
-        cy.get('.item').should('have.length', 20)
-        cy.get('.item')
-          .first()
-          .should('contain', initialTerm)
-        cy.get(`button:contains(${newTerm})`)
-          .should('be.visible')
-      })
 
         it('shows a max of 5 buttons for the last searched terms', () => {
           const faker = require('faker')
@@ -159,7 +181,7 @@ describe('Hacker Stories', () => {
   })
 })
 
-context.only('Errors', () => {
+context('Errors', () => {
   it('shows "Something went wrong ..." in case of a server error', () => {
   cy.intercept(
     'GET',
